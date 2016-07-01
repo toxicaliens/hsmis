878eb56d535dd13a1075db895e23fa12fa8ff729
Bruno Wabwire <bruno.wabwire@obulexsolutions.com>
2016-07-01 15:13:00 +0300

    layouts

--- resources/views/layouts/wizard.blade.php
+++ resources/views/layouts/wizard.blade.php
@@ -1,1 +1,206 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title') | Test</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Select2 -->
+    <link href="assets/select2/dist/css/select2.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="css/custom.css" rel="stylesheet">
+    <link href="assets/bootstrap-fileupload/bootstrap-fileupload.css" rel="stylesheet">
+
+    @stack('css')
+  </head>
+
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <!-- menu profile quick info -->
+            @include('layouts.includes.quickinfo')
+            <!-- /menu profile quick info -->
+            
+            <br />
+            
+           <!-- sidebar menu -->
+            @include('layouts.includes.sidebar_menu')
+            <!-- /sidebar menu -->
+
+            <!-- /menu footer buttons -->
+            @include('layouts.includes.menu_footer_buttons')
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+         <!-- top navigation -->
+        @include('layouts.includes.top_nav')
+        <!-- /top navigation -->
+
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>
+                      @yield('page-title')
+                      <small>
+                          @yield('page-desc')
+                      </small>
+                  </h3>
+              </div>
+
+             @include('layouts.includes.search')
+            </div>
+            <div class="clearfix"></div>
+
+            <div class="row">
+            	<div class="col-md-12 col-sm-12 col-xs-12">
+               	 	<div class="x_panel">
+						@yield('content')
+					</div>              
+        		</div>
+        	</div>
+        <!-- /page content -->
+
+        <!-- footer content -->
+        @include('layouts.includes.footer')
+        <!-- /footer content -->
+      </div>
+    </div>
+
+    <!-- jQuery -->
+    <script src="assets/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap -->
+    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src="assets/fastclick/lib/fastclick.js"></script>
+    <!-- NProgress -->
+    <script src="assets/nprogress/nprogress.js"></script>
+    <!-- jQuery Smart Wizard -->
+    <script src="assets/jQuery-Smart-Wizard/js/jquery.smartWizard.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src="js/custom.js"></script>
+    <!-- validator -->
+    <script src="assets/validator/validator.min.js"></script>
+
+    <!-- bootstrap-daterangepicker -->
+    <script src="js/moment/moment.min.js"></script>
+    <script src="js/datepicker/daterangepicker.js"></script>
+
+    <!-- Select2 -->
+    <script src="assets/select2/dist/js/select2.full.min.js"></script>
+    <!-- validator -->
+    <script src="assets/validator/validator.min.js"></script>
+
+    <!-- custom js -->
+    <script type="text/javascript" src="assets/bootstrap-fileupload/bootstrap-fileupload.js"></script>
+
+    <!-- jQuery Smart Wizard -->
+    <script>
+      $(document).ready(function() {
+        $('#wizard').smartWizard();
+
+        $('#wizard_verticle').smartWizard({
+          transitionEffect: 'slide'
+        });
+
+        $('.buttonNext').addClass('btn btn-success');
+        $('.buttonPrevious').addClass('btn btn-primary');
+        $('.buttonFinish').addClass('btn btn-default');
+      });
+    </script>
+    <!-- /jQuery Smart Wizard -->
+
+    <script>
+      $(document).ready(function() {
+        $('#single_cal1').daterangepicker({
+          singleDatePicker: true,
+          calender_style: "picker_1"
+        }, function(start, end, label) {
+          console.log(start.toISOString(), end.toISOString(), label);
+        });
+        $('#single_cal2').daterangepicker({
+          singleDatePicker: true,
+          calender_style: "picker_2"
+        }, function(start, end, label) {
+          console.log(start.toISOString(), end.toISOString(), label);
+        });
+        $('#single_cal3').daterangepicker({
+          singleDatePicker: true,
+          calender_style: "picker_3"
+        }, function(start, end, label) {
+          console.log(start.toISOString(), end.toISOString(), label);
+        });
+        $('#single_cal4').daterangepicker({
+          singleDatePicker: true,
+          calender_style: "picker_4"
+        }, function(start, end, label) {
+          console.log(start.toISOString(), end.toISOString(), label);
+        });
+      });
+    </script>
+
+    <!-- Select2 -->
+    <script>
+      $(document).ready(function() {
+        $(".select2_single").select2({
+          placeholder: "Select an Option",
+          allowClear: true
+        });
+        $(".select2_group").select2({});
+        $(".select2_multiple").select2({
+          maximumSelectionLength: 4,
+          placeholder: "With Max Selection limit 4",
+          allowClear: true
+        });
+      });
+    </script>
+    <!-- /Select2 -->
+
+    <!-- validator -->
+    <script>
+      // initialize the validator function
+      validator.message.date = 'not a real date';
+
+      // validate a field on "blur" event, a 'select' on 'change' event & a '.reuired' classed multifield on 'keyup':
+      $('form')
+        .on('blur', 'input[required], input.optional, select.required', validator.checkField)
+        .on('change', 'select.required', validator.checkField)
+        .on('keypress', 'input[required][pattern]', validator.keypress);
+
+      $('.multi.required').on('keyup blur', 'input', function() {
+        validator.checkField.apply($(this).siblings().last()[0]);
+      });
+
+      $('form').submit(function(e) {
+        e.preventDefault();
+        var submit = true;
+
+        // evaluate the form using generic validaing
+        if (!validator.checkAll($(this))) {
+          submit = false;
+        }
+
+        if (submit)
+          this.submit();
+
+        return false;
+      });
+    </script>
+    <!-- /validator -->
+
+     @stack('scripts')
+  </body>
+</html>
