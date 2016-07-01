a8114816bbfa688f6ec0e404b7b94ee53a1feb3d
Erico79 <emurinyo@gmail.com>
2016-07-01 13:39:54 +0300

    Controller and Layout Changes

--- resources/views/layouts/includes/sidebar_menu.blade.php
+++ resources/views/layouts/includes/sidebar_menu.blade.php
@@ -4,7 +4,7 @@
                 <ul class="nav side-menu">
                   <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="{{ url('/') }}">Dashboard</a>
+                      <li><a href="{{ url('/dashboard') }}">Dashboard</a>
                       </li>
                     </ul>
                   </li>
